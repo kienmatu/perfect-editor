97c1496f19f3deedf78e4264fa7f6110a85fe129
Kien Dinh <kienmatu1@gmail.com>
2023-07-16 11:58:09 +0700

    Up editor

---
+++ components/RichEditor/RichEditor.tsx
@@ -0,0 +1,88 @@
+import { RichTextEditor, Link } from '@mantine/tiptap';
+import { useEditor } from '@tiptap/react';
+import Highlight from '@tiptap/extension-highlight';
+import StarterKit from '@tiptap/starter-kit';
+import Underline from '@tiptap/extension-underline';
+import TextAlign from '@tiptap/extension-text-align';
+import Superscript from '@tiptap/extension-superscript';
+import SubScript from '@tiptap/extension-subscript';
+import { useEffect, useState } from 'react';
+
+export interface RichEditorProps {
+  isAnalyzeButtonClicked: boolean;
+}
+const content = `<h2 style="text-align: center;">Welcome to Perfect editor</h2>
+<p><code>Perfect editor</code> Support indicating the duplication words. </p>`;
+
+export function RichEditor(props: RichEditorProps) {
+  const initEditor = useEditor({
+    extensions: [
+      StarterKit,
+      Underline,
+      Link,
+      Superscript,
+      SubScript,
+      Highlight,
+      TextAlign.configure({ types: ['heading', 'paragraph'] }),
+    ],
+    content: content,
+  });
+  const [editor, setEditor] = useState(initEditor);
+
+  useEffect(() => {
+    setEditor(initEditor);
+  }, []);
+
+  useEffect(() => {
+    if (props.isAnalyzeButtonClicked) {
+      const json = editor?.state.toJSON();
+      console.log(json);
+    }
+  }, [props.isAnalyzeButtonClicked]);
+
+  return (
+    <RichTextEditor editor={editor}>
+      <RichTextEditor.Toolbar stickyOffset={60}>
+        <RichTextEditor.ControlsGroup>
+          <RichTextEditor.Bold />
+          <RichTextEditor.Italic />
+          <RichTextEditor.Underline />
+          <RichTextEditor.Strikethrough />
+          <RichTextEditor.ClearFormatting />
+          <RichTextEditor.Highlight />
+          <RichTextEditor.Code />
+        </RichTextEditor.ControlsGroup>
+
+        <RichTextEditor.ControlsGroup>
+          <RichTextEditor.H1 />
+          <RichTextEditor.H2 />
+          <RichTextEditor.H3 />
+          <RichTextEditor.H4 />
+        </RichTextEditor.ControlsGroup>
+
+        <RichTextEditor.ControlsGroup>
+          <RichTextEditor.Blockquote />
+          <RichTextEditor.Hr />
+          <RichTextEditor.BulletList />
+          <RichTextEditor.OrderedList />
+          <RichTextEditor.Subscript />
+          <RichTextEditor.Superscript />
+        </RichTextEditor.ControlsGroup>
+
+        <RichTextEditor.ControlsGroup>
+          <RichTextEditor.Link />
+          <RichTextEditor.Unlink />
+        </RichTextEditor.ControlsGroup>
+
+        <RichTextEditor.ControlsGroup>
+          <RichTextEditor.AlignLeft />
+          <RichTextEditor.AlignCenter />
+          <RichTextEditor.AlignJustify />
+          <RichTextEditor.AlignRight />
+        </RichTextEditor.ControlsGroup>
+      </RichTextEditor.Toolbar>
+
+      <RichTextEditor.Content />
+    </RichTextEditor>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,33 @@
-import { Welcome } from '../components/Welcome/Welcome';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+import { Title, Text, Button, Group } from '@mantine/core';
+import { Grid } from '@mantine/core';
+import { RichEditor } from '../components/RichEditor';
+import { useState } from 'react';
 
 export default function HomePage() {
+  const [isButtonClicked, setIsButtonClicked] = useState(false);
+
+  const handleButtonClick = () => {
+    setIsButtonClicked(!isButtonClicked);
+  };
   return (
     <>
-      <Welcome />
-      <ColorSchemeToggle />
+      <Grid style={{ maxWidth: '100%' }}>
+        <Grid.Col span="auto" mt="xl" ml="xl" align="center">
+          <Text inherit variant="gradient" component="span">
+            Perfect Editor
+          </Text>
+        </Grid.Col>
+        <Grid.Col span={6} mt="xl" ml="xl">
+          <RichEditor isAnalyzeButtonClicked={false}></RichEditor>
+        </Grid.Col>
+        <Grid.Col span="auto">
+          <ColorSchemeToggle />
+          <Group position="center" mt="xl">
+            <Button onClick={handleButtonClick}>Analyze</Button>
+          </Group>
+        </Grid.Col>
+      </Grid>
     </>
   );
 }
