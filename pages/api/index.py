f5640bd04c688dc968aeae05533eb7f046550bd1
Kien Dinh <kienmatu1@gmail.com>
2023-07-28 11:37:49 +0700

    update route

--- pages/api/index.py
+++ pages/api/index.py
@@ -5,13 +5,15 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/tok', methods=['POST'])
+@app.route('/api/tok', methods=['POST'])
 def tokenize():
     request_data = request.get_json()
     tokens = word_tokenize(request_data["text"])
     return {"tokens": tokens}
 
 if __name__ == '__main__':
+    # just init
+    word_tokenize("Kiên rất đẹp trai")
     app.run(port=5328)
 
 
