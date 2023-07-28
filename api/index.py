5bc7c4df36fb4e27487b3d6675b583ac813b3cab
Kien Dinh <kienmatu1@gmail.com>
2023-07-28 12:47:40 +0700

    Use fastapi instead of flask

--- api/index.py
+++ api/index.py
@@ -1,33 +1,34 @@
-# from fastapi import FastAPI
-# from pydantic import BaseModel
+from fastapi import FastAPI
+from pydantic import BaseModel
 from underthesea import word_tokenize
-from flask import Flask, request
+# from flask import Flask, request
 
-app = Flask(__name__)
 # just init, avoid NonType
 word_tokenize("Kiên rất đẹp trai")
 
-@app.route('/api/tok', methods=['POST'])
-def tokenize():
-    request_data = request.get_json()
-    tokens = word_tokenize(request_data["text"])
-    return {"tokens": tokens}
+# app = Flask(__name__)
 
-if __name__ == '__main__':
-    # just init
-    word_tokenize("Kiên rất đẹp trai")
-    app.run(port=5328)
-
-
-
-# app = FastAPI()
-
-
-# class Payload(BaseModel):
-#     text: str
-
-
-# @app.post("/tok")
-# def tokenize(req: Payload):
-#     tokens = word_tokenize(req.text)
+# @app.route('/api/tok', methods=['POST'])
+# def tokenize():
+#     request_data = request.get_json()
+#     tokens = word_tokenize(request_data["text"])
 #     return {"tokens": tokens}
+
+# if __name__ == '__main__':
+#     # just init
+#     word_tokenize("Kiên rất đẹp trai")
+#     app.run(port=5328)
+
+
+
+app = FastAPI()
+
+
+class Payload(BaseModel):
+    text: str
+
+
+@app.post("/tok")
+def tokenize(req: Payload):
+    tokens = word_tokenize(req.text)
+    return {"tokens": tokens}
